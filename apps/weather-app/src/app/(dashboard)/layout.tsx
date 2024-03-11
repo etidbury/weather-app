461a7482455cca9f4dc7c197c70f95b8641ce158
Edd Tidbury <edd.tidbury@gmail.com>
2024-03-11 19:54:06 +0000

    feat: pass theme search param to theme provider context

--- apps/weather-app/src/app/(dashboard)/layout.tsx
+++ apps/weather-app/src/app/(dashboard)/layout.tsx
@@ -1,16 +1,21 @@
 'use client';
 import { ThemeProvider } from 'next-themes';
+import { useSearchParams } from 'next/navigation';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const searchParams = useSearchParams();
+
   return (
     <ThemeProvider
       attribute="class"
       themes={['dark', 'light']}
       defaultTheme="dark"
+      forcedTheme={searchParams?.get('theme') ?? undefined}
+      enableSystem
     >
       {children}
     </ThemeProvider>
