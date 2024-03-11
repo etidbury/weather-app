593411ac13abaf86a55e0c8b6c0486287db41040
Edd Tidbury <edd.tidbury@gmail.com>
2024-03-11 17:51:31 +0000

    feat: config initial cloudfront cache policy

--- stacks/NextJsSiteStack.ts
+++ stacks/NextJsSiteStack.ts
@@ -1,5 +1,11 @@
-import { NextjsSite, StackContext } from "sst/constructs";
-
+import { NextjsSite, StackContext } from 'sst/constructs';
+import { Duration } from 'aws-cdk-lib';
+import {
+  CachePolicy,
+  CacheQueryStringBehavior,
+  CacheHeaderBehavior,
+  CacheCookieBehavior,
+} from 'aws-cdk-lib/aws-cloudfront';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
@@ -7,16 +13,29 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export function NextJsSiteStack({ stack }: StackContext) {
-
   if (!process.env.WEATHER_API_KEY) {
     throw new Error('WEATHER_API_KEY environment variable is required');
   }
 
-  const site = new NextjsSite(stack, "weather-app-web", {
+  const serverCachePolicy = new CachePolicy(stack, 'ServerCache', {
+    queryStringBehavior: CacheQueryStringBehavior.all(),
+    headerBehavior: CacheHeaderBehavior.none(),
+    cookieBehavior: CacheCookieBehavior.none(),
+    defaultTtl: Duration.minutes(1),
+    maxTtl: Duration.minutes(2),
+    minTtl: Duration.minutes(1),
+    enableAcceptEncodingBrotli: true,
+    enableAcceptEncodingGzip: true,
+  });
+
+  const site = new NextjsSite(stack, 'weather-app-web', {
     path: __dirname + '/apps/weather-app/',
     environment: {
       WEATHER_API_KEY: process.env.WEATHER_API_KEY as string,
-    }
+    },
+    cdk: {
+      serverCachePolicy,
+    },
   });
 
   stack.addOutputs({
